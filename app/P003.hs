44d7c00af9451423bbc5acd3cf5f8c46eeae06cd
James Leslie <jamesleslie@posteo.net>
2022-04-08 16:39:43 +0100

    [P004] Solved P004

--- app/P003.hs
+++ app/P003.hs
@@ -1,3 +1,18 @@
 module P003 where
 
-main = print "hi"
+import Data.List
+import Primes
+
+-- * Question
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+-- What is the largest prime factor of the number 600851475143?
+
+-- * Solution
+-- We use the efficient prime generation from the Primes library to
+-- construct a list of all primes. Then, we extract the largest prime
+-- factor from the list of factors.
+
+solution :: Int
+solution = maximum . primeFactors $ 600851475143
+
+main = print solution
---
+++ src/Primes.hs
@@ -0,0 +1,41 @@
+module Primes where
+
+import qualified Data.Map as Map
+import Control.Concurrent (writeChan)
+import Control.Monad.State.Lazy
+
+
+-- * Primes
+-- This is mostly taken from the Data.Numbers.Primes haskell library
+-- and Melissa O'Neil's Genuine Sieve of Eratosthenes.
+
+sieve3 :: (Ord k, Num k) => [k] -> [k]
+sieve3 xs = sieve' xs Map.empty
+  where
+    sieve' [] table = []
+    sieve' (x:xs) table =
+      case Map.lookup x table of
+        Nothing -> x : sieve' xs (Map.insert (x*x) [x] table)
+        Just facts -> sieve' xs (foldl reinsert (Map.delete x table) facts)
+          where
+            reinsert table prime = Map.insertWith (++) (x+prime) [prime] table
+
+
+wheel2357 :: Integral a => [a]
+wheel2357 = 2:4:2:4:6:2:6:4:2:4:6:6:2:6:4:2:6:4:6:8:4:2:4:2:4:8:6:4:6:2:4:6:2:6:6:4:2:4:6:2:6:4:2:4:2:10:2:10:wheel2357
+
+spin :: Num t => [t] -> t -> [t]
+spin (x:xs) n = n : spin xs (n + x)
+
+primes :: Integral a => [a]
+primes = 2:3:5:7: sieve3 (spin wheel2357 11)
+
+-- | Generates a list of prime factors of n, with repetition.
+primeFactors :: Integral a => a -> [a]
+primeFactors n = divtest n primes
+  where
+    divtest 1 _ = []
+    divtest _ [] = []
+    divtest n (x:xs)
+      | n `mod` x == 0 = x : divtest (n `div` x) (x:xs)
+      | otherwise      = divtest n xs
