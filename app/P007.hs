d32aa8b63dbce2847b409cc0b957d92ded06fd4f
James Leslie <jamesleslie@posteo.net>
2022-04-24 22:14:02 +0100

    [P007] Added solution to P007

--- app/P007.hs
+++ app/P007.hs
@@ -1,9 +1,15 @@
 module P007 where
+import Primes
 
 -- * Question
 -- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 -- What is the 10001st prime number?
 
 -- * Solution
+-- We have already constructed an efficient list of primes, in order,
+-- so we use that
 
-main = print "hello"
+p007 :: Int
+p007 = primes !! 10000
+
+main = print p007
