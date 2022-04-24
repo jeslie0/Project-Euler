af839380eaa1f45f37c824092a214dae69163e94
James Leslie <jamesleslie@posteo.net>
2022-04-24 22:13:16 +0100

    [primes] Fixed errors in prime generation and improved algorithm
    We now use a priority queue rather than a map.

--- src/Primes.hs
+++ src/Primes.hs
@@ -1,34 +1,41 @@
 module Primes where
 
-import qualified Data.Map as Map
-import Control.Concurrent (writeChan)
-import Control.Monad.State.Lazy
-
+import qualified Data.PQueue.Prio.Min as P
 
 -- * Primes
--- This is mostly taken from the Data.Numbers.Primes haskell library
--- and Melissa O'Neil's Genuine Sieve of Eratosthenes.
+-- This is taken almost verbatim from Melissa O'Neil's Genuine Sieve
+-- of Eratosthenes paper.
 
-sieve3 :: (Ord k, Num k) => [k] -> [k]
-sieve3 xs = sieve' xs Map.empty
+sieve [] = []
+sieve (x:xs) = x : sieve' xs (insertPrime x xs P.empty)
   where
-    sieve' [] table = []
-    sieve' (x:xs) table =
-      case Map.lookup x table of
-        Nothing -> x : sieve' xs (Map.insert (x*x) [x] table)
-        Just facts -> sieve' xs (foldl reinsert (Map.delete x table) facts)
+    insertPrime p xs = P.insert (p * p) (map (* p) xs)
+    sieve' [] table     = []
+    sieve' (x:xs) table
+      | nextComposite <= x = sieve' xs (adjust table)
+      | otherwise          = x : sieve' xs (insertPrime x xs table)
+      where
+        nextComposite = fst $ P.findMin table
+        adjust table
+          | n <= x    = adjust (deleteMinAndInsert n' ns table)
+          | otherwise = table
           where
-            reinsert table prime = Map.insertWith (++) (x+prime) [prime] table
-
+            (n, n':ns) = P.findMin table
+            deleteMinAndInsert :: (Ord k) => k -> v -> P.MinPQueue k v -> P.MinPQueue k v
+            deleteMinAndInsert k v table = P.insert k v $ P.deleteMin table
 
 wheel2357 :: Integral a => [a]
-wheel2357 = 2:4:2:4:6:2:6:4:2:4:6:6:2:6:4:2:6:4:6:8:4:2:4:2:4:8:6:4:6:2:4:6:2:6:6:4:2:4:6:2:6:4:2:4:2:10:2:10:wheel2357
+wheel2357 =
+  2:4:2:4:6:2:6:4:2:4:6:6:2:6:4:2:6:4:6:8:4:2:4:2:4:8:6:4:6:2:4:6:2:6:6:4:2:4:6:2:6:4:2:4:2:10:2:10:wheel2357
 
 spin :: Num t => [t] -> t -> [t]
 spin (x:xs) n = n : spin xs (n + x)
 
 primes :: Integral a => [a]
-primes = 2:3:5:7: sieve3 (spin wheel2357 11)
+primes = 2:3:5:7: sieve (spin wheel2357 11)
+
+
+-- * Using the list of primes
 
 -- | Generates a list of prime factors of n, with repetition.
 primeFactors :: Integral a => a -> [a]
@@ -40,7 +47,7 @@ primeFactors n = divtest n primes
       | n `mod` x == 0 = x : divtest (n `div` x) (x:xs)
       | otherwise      = divtest n xs
 
-
+-- | Generates a list of primes, with their corresponding factors
 primeFactorPowers :: Integral a => a -> [(a, Int)]
 primeFactorPowers = collect . primeFactors
   where
@@ -50,6 +57,8 @@ primeFactorPowers = collect . primeFactors
       let (ns, remaining) = span (\a -> a == head xs) xs
       in (head ns, length ns) : collect remaining
 
+-- | Converts a list of prime powers to a number. This is the inverse
+-- of the above.
 primePowers2Int :: Integral a => [(a, Int)] -> a
 primePowers2Int [] = 1
 primePowers2Int ((n,i):ns) = n ^ i * primePowers2Int ns
