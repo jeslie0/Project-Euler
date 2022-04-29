af45667247750b7a36653d5d1de1f85b34f20585
James Leslie <jamesleslie@posteo.net>
2022-04-29 15:09:05 +0100

    [P010] Solved P010

--- app/P010.hs
+++ app/P010.hs
@@ -1,10 +1,15 @@
 module P010 where
 
+import Primes
 -- * Question
 -- The sum of the primes below 10 is \(2 + 3 + 5 + 7 = 17\).
 --
 -- Find the sum of all the primes below two million.
 
 -- * Solution
+-- We just sum up the required primes from out list of primes.
 
-main = print "hello"
+p010 :: Int
+p010 = sum . takeWhile (<= 2000000) $ primes
+
+main = print p010
