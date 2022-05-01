4b5b623b86e6cc470bf74033132507e7e5b2d97d
James Leslie <jamesleslie@posteo.net>
2022-05-01 15:58:36 +0100

    [P015] Finished P015

--- app/P015.hs
+++ app/P015.hs
@@ -8,6 +8,21 @@ module P015 where
 -- how many such routes are there through a 20x20 grid?
 
 -- * Solution
+-- For an \(m \times n\) grid, the number of paths from the top left to
+-- the bottom right corner is given by the recurrence relation
+-- \[ r_{m,n} = r_{m-1,n} + r_{m, n-1}, r_{k,1} = r_{1,k} = 1. \]
+-- We could try and solve this, however this a more standard
+-- combinatorial approach. We can think of a path as being a \(m+n\)
+-- list of directions, with exactly \(m\) downs and \(n\) rights. This
+-- problem then becomes figuring out how many ways we can choose \(m\)
+-- from \(n + m\).
 
+fac :: (Integral a) => a -> a
+fac 0 = 1
+fac n = product [1..n]
 
-main = print "hi"
+p015 :: Integer
+p015 = fac 40 `div` (fac 20 * fac 20)
+
+main :: IO ()
+main = print p015
---
+++ app/P016.hs
@@ -0,0 +1,11 @@
+module P016 where
+
+-- * Question
+-- \(2^{15} = 32768\) and the sum of its digits is \(3 + 2 + 7 + 6 + 8
+-- = 26\).
+-- What is the sum of the digits of the number \(2^{1000}\)?
+
+-- * Solution
+
+main :: IO ()
+main = print "hi"
