a93279f4440da6c15a6f4ce80ea808628769ca5e
James Leslie <jamesleslie@posteo.net>
2022-05-03 21:52:40 +0100

    [P019] Added solution to P019

--- app/P019.hs
+++ app/P019.hs
@@ -1,5 +1,5 @@
 module P019 where
-
+import GHC.Utils.Misc (count)
 -- * Question
 -- You are given the following information, but you may prefer to do
 -- some research for yourself.
@@ -17,7 +17,7 @@ module P019 where
 -- How many Sundays fell on the first of the month during the
 -- twentieth century (1 Jan 1901 to 31 Dec 2000)?
 -- * Solution
-
+-- | A data type for the days of the week
 data Days = Mon
           | Tue
           | Wed
@@ -27,6 +27,7 @@ data Days = Mon
           | Sun
           deriving (Eq, Show)
 
+-- | We can enumerate the days of the week
 instance Enum Days where
   fromEnum Mon = 1
   fromEnum Tue = 2
@@ -46,15 +47,7 @@ instance Enum Days where
     | n == 0 = Sun
     | otherwise = toEnum (n `mod` 7)
 
-nextDay :: Days -> Days
-nextDay Mon = Tue
-nextDay Tue = Wed
-nextDay Wed = Thu
-nextDay Thu = Fri
-nextDay Fri = Sat
-nextDay Sat = Sun
-nextDay Sun = Mon
-
+-- | A datatype for the months in a year
 data Month = Jan
            | Feb
            | Mar
@@ -69,13 +62,15 @@ data Month = Jan
            | Dec
            deriving (Eq, Show, Enum)
 
+-- | A type alias for the year
 type Year = Int
 
-monthLength :: Year -> Month -> Int
+-- | The month length, where the boolean input is True if and only if
+-- the year is a leap year
+monthLength :: Bool -> Month -> Int
 monthLength _ Jan = 31
-monthLength yr Feb
-  | leapYear yr = 29
-  | otherwise   = 28
+monthLength True Feb  = 29
+monthLength False Feb = 28
 monthLength _ Mar = 31
 monthLength _ Apr = 30
 monthLength _ May = 31
@@ -87,6 +82,7 @@ monthLength _ Oct = 31
 monthLength _ Nov = 30
 monthLength _ Dec = 31
 
+
 -- | A predicate determining if a year is a leap year
 leapYear :: Year -> Bool
 leapYear n
@@ -95,17 +91,35 @@ leapYear n
   | n `mod` 4 == 0   = True
   | otherwise        = False
 
-startDay :: Year -> Days
-startDay 1900 = Mon
-startDay n
-  | leapYear n = succ . succ . startDay $ n - 1
-  | otherwise  = succ . startDay $ n - 1
+-- | Given the starting day of the year, and whether or not it is a
+-- leap year, output the number of sundays that start a month. We
+-- calculate a list of the number of days in each month, modulo 7,
+-- then form a list of the starting day of each month, from which we
+-- count Sundays.
+sundaysOnStartOfMonth :: Days -> Bool -> Int
+sundaysOnStartOfMonth day bool =
+  count (== Sun) $
+  scanl (\d n -> compose succ n d) day $
+  map ((\n -> mod n 7) . monthLength bool) [Jan .. Nov]
 
--- sundaysOnStartOfMonth :: Year -> Int
--- sundaysOnStartOfMonth yr
---   | leapYear yr = _
---   | otherwise = _
+-- | Gives the n-fold composite of f
+compose :: (a -> a) -> Int -> a -> a
+compose f 0 = \x -> x
+compose f n = f . compose f (n-1)
 
--- p019 =
+-- | Generates a list of the year starting days and a boolean which
+-- tells us if that year is a leap year or not
+listOfStartDays :: [(Days, Bool)]
+listOfStartDays = take 100 (genList' 1901 Mon)
+  where
+    genList' yr day
+      | leapYear (yr - 1) = (compose succ (366 `mod` 7) day, leapYear yr)  : genList' (yr + 1) (compose succ (366 `mod` 7) day)
+      | otherwise   = (compose succ (365 `mod` 7) day, leapYear yr) : genList' (yr + 1) (compose succ (365 `mod` 7) day)
 
-main = print "hi"
+-- | We map the number of sundaysOnStartOfMonth over the
+-- listOfStartDays, then sum to get the final answer.
+p019 :: Int
+p019 = sum $ map (\(day,bool) -> sundaysOnStartOfMonth day bool) listOfStartDays
+
+main :: IO ()
+main = print p019
